5d1f81ac8afef3724c5b48f5034baf44e3c9ff3c
Viktor Lust <v.lust2001@gmail.com>
2022-04-10 03:31:49 +0200

    cleanup

--- testing_files/test/testing_list_comp.py
+++ testing_files/test/testing_list_comp.py
@@ -1,6 +1,11 @@
-from random import uniform
+from random import uniform, choice
+import numpy as np
 
-def add_random_cone(max_x, max_y, n):
-        r_cones=[(uniform(0, max_x), uniform(0, max_y)) for _ in range(n)]
-        return r_cones
-print(add_random_cone(10, 10, 5))
+# def add_random_cone(max_x, max_y, n):
+#         r_cones=[(uniform(0, max_x), uniform(0, max_y)) for _ in range(n)]
+#         return r_cones
+# print(add_random_cone(10, 10, 5))
+
+# x=0.99
+# if not np.random.choice([0,1],p=[x, 1-x]):
+#                         print("this work?")
